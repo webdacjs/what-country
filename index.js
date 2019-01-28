1828eb7411aeddd07ecdcb0283780a73f4d93477
webdacjs <me@jco.im>
2019-01-28 23:30:47 +0100

    Addding additional query modes.

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ const {getFilterField} = require('./utils')
 
 const lower = str => String(str.toLowerCase())
 
+const queryCurrency = q => {
+  const fieldFilter = q.length === 3 ? 'CurrencyCode' : 'CurrencyName'
+  return countries.filter(x => lower(x[fieldFilter]) === lower(q))
+}
+
+const queryPhone = q => {
+  return countries.filter(x => parseInt(x.Phone) === parseInt(q))
+}
+
 const query = q => {
   const fieldFilter = getFilterField(q)
   return countries.filter(x => lower(x[fieldFilter]) === lower(q))
 }
 
 module.exports = {
+  queryCurrency,
+  queryPhone,
   query
 }
